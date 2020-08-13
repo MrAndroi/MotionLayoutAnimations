8c793d2feafd553985be522b92ea2e19e5da16f3
sami shorman <sami.shorman3000@gmail.com>
2020-08-13 02:57:21 -0700

    first commit

---
+++ app/src/main/java/com/rodrigodominguez/mixanimationsmotionlayout/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.rodrigodominguez.mixanimationsmotionlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rodrigodominguez.mixanimationsmotionlayout.circularcards.CircularCardsHomeActivity
+import com.rodrigodominguez.mixanimationsmotionlayout.examplekeyposition.PositionKeyExampleActivity
+import com.rodrigodominguez.mixanimationsmotionlayout.rotationcard.RotationCardDemoActivity
+import com.rodrigodominguez.mixanimationsmotionlayout.rotationcard.RotationCardHomeActivity
+import com.rodrigodominguez.mixanimationsmotionlayout.storiesinstagram.InstagramStoryHomeActivity
+import com.rodrigodominguez.mixanimationsmotionlayout.verticalstackcards.VerticalStackCardsHomeActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        storiesHome.setOnClickListener {
+            startActivity(
+                Intent(this, InstagramStoryHomeActivity::class.java)
+            )
+        }
+        circularCards.setOnClickListener {
+            startActivity(
+                Intent(this, CircularCardsHomeActivity::class.java)
+            )
+        }
+        rotationCardScenes.setOnClickListener {
+            startActivity(
+                Intent(this, RotationCardHomeActivity::class.java)
+            )
+        }
+
+        verticalCards.setOnClickListener {
+            startActivity(
+                Intent(this, VerticalStackCardsHomeActivity::class.java)
+            )
+        }
+        positionKeyExamples.setOnClickListener {
+            startActivity(
+                Intent(this, PositionKeyExampleActivity::class.java)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/rodrigodominguez/mixanimationsmotionlayout/verticalstackcards/Card.kt
@@ -0,0 +1,7 @@
+package com.rodrigodominguez.mixanimationsmotionlayout.verticalstackcards
+
+import androidx.annotation.ColorInt
+
+data class Card(
+    @ColorInt val backgroundColor: Int
+)
